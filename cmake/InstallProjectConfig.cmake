c3551d3c2cff7e2e6a9689b08a5e6f3b8bccfdcf
Abhijit Kundu <abhijit.kundu@gatech.edu>
2014-12-01 20:36:12 -0500

    in middle of ProjectConfig installation

---
+++ cmake/InstallProjectConfig.cmake
@@ -0,0 +1,19 @@
+###################### InstallProjectConfig  ###########################
+
+include(CMakePackageConfigHelpers)
+configure_package_config_file(
+  ${CMAKE_SOURCE_DIR}/cmake/${PROJECT_NAME}Config.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+  INSTALL_DESTINATION ${INSTALL_CMAKE_DIR}
+  PATH_VARS INSTALL_INCLUDE_DIR
+  )
+
+install(
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+  DESTINATION ${INSTALL_CMAKE_DIR}
+  COMPONENT Devel
+  )
+
+install(EXPORT ${PROJECT_NAME}Targets 
+  DESTINATION ${INSTALL_CMAKE_DIR}
+  )
